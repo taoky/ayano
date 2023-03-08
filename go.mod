acfb8cfdf9e4d37a4cd2286a65ea46edd848c592
taoky <me@taoky.moe>
2023-03-08 20:35:51 +0800

    fix: TailFile starts from offset 0 instead of end
    - use nxadm/tail as hpcloud/tail has no more maintenance
    - add CompleteLines in tailfile config
    - eat first line as it cannot ensure if the first line it got is complete

--- go.mod
+++ go.mod
@@ -4,12 +4,11 @@ go 1.20
 
 require (
 	github.com/dustin/go-humanize v1.0.1
-	github.com/hpcloud/tail v1.0.0
+	github.com/nxadm/tail v1.4.9-0.20211216163028-4472660a31a6
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
-	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- main.go
+++ main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"math"
 	"net/netip"
 	"sort"
 	"sync"
 	"time"
 
 	"github.com/dustin/go-humanize"
-	"github.com/hpcloud/tail"
+	"github.com/nxadm/tail"
 )
 
 var sizeStats map[string]uint64
 var reqStats map[string]int
 var lastURL map[string]string
+var lastURLUpdateDate map[string]time.Time
 var statLock sync.Mutex
 
 func printTopValues() {
@@ -31,14 +34,19 @@ func printTopValues() {
 			return sizeStats[keys[i]] > sizeStats[keys[j]]
 		})
 		// print top 10
-		for i := 0; i < 10; i++ {
+		top := 10
+		if len(keys) < 10 {
+			top = len(keys)
+		}
+		for i := 0; i < top; i++ {
 			key := keys[i]
 			total := sizeStats[key]
 			reqTotal := reqStats[key]
 			last := lastURL[key]
+			lastTime := lastURLUpdateDate[key].Format("2006-01-02 15:04:05")
 
 			average := total / uint64(reqTotal)
-			log.Printf("%s: %s %d %s %s\n", key, humanize.Bytes(total), reqTotal, humanize.Bytes(average), last)
+			log.Printf("%s: %s %d %s %s (%s)\n", key, humanize.Bytes(total), reqTotal, humanize.Bytes(average), last, lastTime)
 		}
 		fmt.Println()
 		statLock.Unlock()
@@ -49,19 +57,30 @@ func main() {
 	sizeStats = make(map[string]uint64)
 	reqStats = make(map[string]int)
 	lastURL = make(map[string]string)
+	lastURLUpdateDate = make(map[string]time.Time)
 	t, err := tail.TailFile("/var/log/nginx/mirrors/access_json.log", tail.Config{
 		Follow: true,
 		ReOpen: true,
+		Location: &tail.SeekInfo{
+			Offset: -1024 * 1024,
+			Whence: io.SeekEnd,
+		},
+		CompleteLines: true,
 	})
 	if err != nil {
 		panic(err)
 	}
 	go printTopValues()
+
+	// Eat a line from t.Lines, as first line may be incomplete
+	<-t.Lines
+
 	for line := range t.Lines {
 		var logItem map[string]any
 		err := json.Unmarshal([]byte(line.Text), &logItem)
 		if err != nil {
 			log.Printf("json unmarshal error: %v\n", err)
+			log.Printf("got line: %s\n", line.Text)
 			continue
 		}
 		size := uint64(logItem["size"].(float64))
@@ -92,7 +111,12 @@ func main() {
 		} else {
 			reqStats[clientPrefix.String()] = 1
 		}
-		lastURL[clientPrefix.String()] = logItem["url"].(string)
+		url := logItem["url"].(string)
+		if url != lastURL[clientPrefix.String()] {
+			lastURL[clientPrefix.String()] = url
+			sec, dec := math.Modf(logItem["timestamp"].(float64))
+			lastURLUpdateDate[clientPrefix.String()] = time.Unix(int64(sec), int64(dec*1e9))
+		}
 		statLock.Unlock()
 	}
 }
