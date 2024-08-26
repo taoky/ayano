be79b701952761f56d2acdf4895b5af87a57e2c2
iBug <git@ibugone.com>
2024-08-26 20:12:46 +0800

    Add back one missing test

--- pkg/parser/nginx-combined_test.go
+++ pkg/parser/nginx-combined_test.go
@@ -1,6 +1,31 @@
 package parser
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
+
+func TestNginxCombinedParser(t *testing.T) {
+	p := NginxCombinedParser{}
+	line := `123.45.67.8 - - [12/Mar/2023:00:15:32 +0800] "GET /path/to/a/file HTTP/1.1" 200 3009 "-" ""`
+	log, err := p.Parse([]byte(line))
+	if err != nil {
+		t.Error(err)
+	}
+	if log.Size != 3009 {
+		t.Errorf("expected size 3009, got %d", log.Size)
+	}
+	if log.Client != "123.45.67.8" {
+		t.Errorf("expected client 123.45.67.8, got %s", log.Client)
+	}
+	if log.URL != "/path/to/a/file" {
+		t.Errorf("expected url /path/to/a/file, got %s", log.URL)
+	}
+	expectedTime := time.Date(2023, 3, 12, 0, 15, 32, 0, time.FixedZone("CST", 8*60*60))
+	if expectedTime.Sub(log.Time).Abs() > time.Microsecond {
+		t.Errorf("expected time %v, got %v", expectedTime, log.Time)
+	}
+}
 
 func TestNginxCombinedParserWithUnusualInputs(t *testing.T) {
 	p := NginxCombinedParser{}
